91de32d37536fc366ec85cd665232d16d3d886d6
simonhaenisch <simonhaenisch@users.noreply.github.com>
2020-07-28 22:40:32 +0200

    fix: `verifyToken` also sets same-site cookie options for Chrome

--- src/verify-request/verify-token.ts
+++ src/verify-request/verify-token.ts
@@ -1,5 +1,6 @@
 import { Header, Method, StatusCode } from '@shopify/network';
 import { Context } from 'koa';
+import getCookieOptions from '../auth/cookie-options';
 import { TEST_COOKIE_NAME, TOP_LEVEL_OAUTH_COOKIE_NAME } from '../index';
 import { NextFunction } from '../types';
 import { Routes } from './types';
@@ -10,7 +11,7 @@ export function verifyToken(routes: Routes) {
 		const { session } = ctx;
 
 		if (session && session.accessToken) {
-			ctx.cookies.set(TOP_LEVEL_OAUTH_COOKIE_NAME);
+			ctx.cookies.set(TOP_LEVEL_OAUTH_COOKIE_NAME, '', getCookieOptions(ctx));
 			// If a user has installed the store previously on their shop, the accessToken can be stored in session.
 			// we need to check if the accessToken is valid, and the only way to do this is by hitting the api.
 			const response = await fetch(`https://${session.shop}/admin/metafields.json`, {
@@ -30,7 +31,7 @@ export function verifyToken(routes: Routes) {
 			return;
 		}
 
-		ctx.cookies.set(TEST_COOKIE_NAME, '1');
+		ctx.cookies.set(TEST_COOKIE_NAME, '1', getCookieOptions(ctx));
 
 		redirectToAuth(routes, ctx);
 	};
